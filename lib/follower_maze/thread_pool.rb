be6a71fcbac533222f142db376d7133e938caa02
Theodore Konukhov <komolov.f@gmail.com>
2014-09-24 04:15:39 +0200

    memory leak fixed

--- lib/follower_maze/thread_pool.rb
+++ lib/follower_maze/thread_pool.rb
@@ -7,7 +7,6 @@ module FollowerMaze
     def initialize
       @concurrency = 25 # should be in config
       @mutex = Mutex.new
-      @threads = []
       @working = {}
       @cond    = ConditionVariable.new
       @waiting = Queue.new
@@ -22,37 +21,31 @@ module FollowerMaze
 
     def work
       while @waiting.size > 0
-        thread = Thread.new do
-          context, block, id = get_work
-
+        thread = Thread.new do |t|
+          context, block, id = get_work_for(thread)
           block.call context
 
           @mutex.synchronize do
             @working.delete id
-            @threads.delete thread
           end
         end
 
-        @mutex.synchronize do
-          @threads << thread
-        end
-
         if @working.size == @concurrency
-          @threads.map &:join
+          @working.values.map &:join
         end
       end
     end
 
-    def get_work(put_back = nil)
+    def get_work_for(thread, put_back = nil)
       work  = @waiting.pop
       id    = work[2]
 
       @waiting.push put_back if put_back
 
       if @working[id]
-        get_work(work)
+        get_work_for(thread, work)
       else
-        @mutex.synchronize { @working[id] = true }
+        @mutex.synchronize { @working[id] = thread }
         return work
       end
     end
