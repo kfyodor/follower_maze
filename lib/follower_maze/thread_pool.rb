ca3cf6c7d589734905ff5ea81ed1274b9d6451be
Theodore Konukhov <komolov.f@gmail.com>
2014-09-24 03:26:49 +0200

    fixed bug in thread pool

--- lib/follower_maze/thread_pool.rb
+++ lib/follower_maze/thread_pool.rb
@@ -5,12 +5,14 @@ module FollowerMaze
     attr_reader :working, :waiting, :threads
 
     def initialize
-      @concurrency = 10 # should be in config
+      @concurrency = 25 # should be in config
       @mutex = Mutex.new
       @threads = []
       @working = {}
       @cond    = ConditionVariable.new
       @waiting = Queue.new
+
+      @recursion_count = 0
     end
 
     def add_work(context, id, &block)
@@ -20,18 +22,20 @@ module FollowerMaze
 
     def work
       while @waiting.size > 0
-        thread = Thread.new(get_work) do |work|
-          context, block, id = work
-          mutex              = Mutex.new
+        thread = Thread.new do
+          context, block, id = get_work
 
           block.call context
 
-          mutex.synchronize do
-            @working[id] = nil
+          @mutex.synchronize do
+            @working.delete id
             @threads.delete thread
           end
         end
-        @threads << thread
+
+        @mutex.synchronize do
+          @threads << thread
+        end
 
         if @working.size == @concurrency
           @threads.map &:join
@@ -42,14 +46,13 @@ module FollowerMaze
     def get_work(put_back = nil)
       work  = @waiting.pop
       id    = work[2]
-      mutex = Mutex.new
 
       @waiting.push put_back if put_back
 
       if @working[id]
         get_work(work)
       else
-        mutex.synchronize { @working[id] = true }
+        @mutex.synchronize { @working[id] = true }
         return work
       end
     end
